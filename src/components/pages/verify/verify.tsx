1cd28d0d8735f5a56d164de87fed21158c51ece0
Alli Samuel <samueltemiloluwaalli@gmail.com>
2022-11-13 10:42:35 +0100

    fixed verify page

--- src/components/pages/verify/verify.tsx
+++ src/components/pages/verify/verify.tsx
@@ -29,7 +29,7 @@ export const Verify = () => {
             src={back}
             alt="go to dashboard"
             className="inline"
-            onClick={() => navigate("/")}
+            onClick={() => navigate("/dashboard")}
           />
         </p>
       </div>
